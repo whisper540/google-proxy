aae2b5f134d4f3fd5d22d6e3f1eb0890362d18cb
Mr.吴 <761448541@qq.com>
2023-05-19 17:54:06 +0800

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { Server } from "https://deno.land/std/http/server.ts";
 
-const TARGET = "www.youtube.com";
+const TARGET = "www.google.com";
 const handler = async (request: Request) => {
   const url = new URL(request.url);
   const targetUrl = new URL(TARGET + url.pathname + url.search);
